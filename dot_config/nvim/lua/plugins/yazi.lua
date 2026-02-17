295053416c45c7afa80dce7baaf6109a01376526
atnook <atriumnook@gmail.com>
2026-02-18 07:00:29 +0900

    feat: Neovim(LazyVim), starship, zsh, VSCode, mise設定を追加

---
+++ dot_config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,45 @@
+-- ==========================================================
+-- yazi.nvim — Terminal file manager integration
+-- ==========================================================
+-- Requires: brew install yazi
+-- https://github.com/mikavilpas/yazi.nvim
+return {
+  {
+    "mikavilpas/yazi.nvim",
+    version = "*",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", lazy = true },
+    },
+    keys = {
+      {
+        "<leader>-",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    ---@type YaziConfig | {}
+    opts = {
+      open_for_directories = true,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+    init = function()
+      -- disable netrw when using open_for_directories=true
+      -- https://github.com/mikavilpas/yazi.nvim/issues/802
+      vim.g.loaded_netrwPlugin = 1
+    end,
+  },
+}
