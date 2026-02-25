570cd6695bb72efe293eba41d50bcd33b99d0dba
atnook <atriumnook@gmail.com>
2026-02-26 00:39:31 +0900

    dotfiles全体のバグ修正と設定改善
    - gitconfig: 重複した[core]セクションを統合
    - gitconfig: pull.rebase, push.autoSetupRemote, fetch.prune, init.defaultBranch を追加
    - zshrc: SHARE_HISTORYと競合するINC_APPEND_HISTORYを削除
    - tmux: WSLでclip.exeの代わりにOSC 52を使用（UTF-8文字化け対策）
    - nvim: 廃止されたneodev設定をdot_neoconf.jsonから削除
    - nvim: yazi.nvimの不要なplenary.nvim依存を削除

--- dot_config/nvim/lua/plugins/yazi.lua
+++ dot_config/nvim/lua/plugins/yazi.lua
@@ -8,9 +8,6 @@ return {
     "mikavilpas/yazi.nvim",
     version = "*",
     event = "VeryLazy",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", lazy = true },
-    },
     keys = {
       {
         "<leader>-",
